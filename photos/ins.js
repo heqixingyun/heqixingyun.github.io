aa3a1b3b72d2bcfff776e4bd9f5adeafab616b0b
heqixingyun <184874582@qq.com>
2020-03-14 14:04:58 +0800

    Site updated: 2020-03-14 14:04:58

--- photos/ins.js
+++ photos/ins.js
@@ -117,8 +117,8 @@
                 var data = res.list[j].arr;
                 var liTmpl = "";
                 for (var i = 0, len = data.link.length; i < len; i++) {
-                    var minSrc = 'https://github.com/heqixingyun/LR/blob/master/LR/min_photos/' + data.link[i];
-                    var src = 'https://github.com/heqixingyun/LR/blob/master/LR/photos/' + data.link[i];
+                    var minSrc = 'https://github.com/heqixingyun/LR/min_photos/' + data.link[i];
+                    var src = 'https://github.com/heqixingyun/LR/photos/' + data.link[i];
                     var type = data.type[i];
                     var target = src + (type === 'video' ? '.mp4' : '.jpg');
                     src += '';
